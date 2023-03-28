e817d1892b7f9ff3f01412d0ec7d453733fd8fa5
Sgiuri <simone.giuri@gmail.com>
2023-03-28 22:09:19 +0200

    Preparazione Lezione 8 ter

--- Lezione8/myFlaskBlog/myflaskblog/forms.py
+++ Lezione8/myFlaskBlog/myflaskblog/forms.py
@@ -22,31 +22,31 @@ email
 password
 remembre_me
 '''
-
-
 class RegistrationForm(FlaskForm):
+    # Campo per l'username con lunghezza minima e massima, non può essere vuoto
     username = StringField('Username', validators=[
         Length(min=2, max=30), DataRequired()])
-    #    email = EmailField('email', validators=[Length(2, 100)])
+    # Campo per l'email con lunghezza minima e massima, deve essere una email valida, non può essere vuoto
     email = StringField('Email', validators=[
         Length(min=2, max=100), Email(), DataRequired()])
+    # Campo per la password con lunghezza minima e massima, non può essere vuoto
     password = PasswordField('Password', validators=[
         Length(min=8, max=100), DataRequired()])
+    # Campo per la conferma della password con lunghezza minima e massima, deve essere uguale alla password, non può essere vuoto
     confirm_password = PasswordField('Confirm Password',
                                      validators=[Length(min=8, max=100),
                                                  DataRequired(),
                                                  EqualTo('password')])
+    # Bottone per registrarsi
     submit = SubmitField('Register Now!')
 
-    # def validate_field(self, field):
-    #     if True:
-    #         raise ValdationError('Validation Message')
-
+    # Validazione email unica
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Email already registered')
 
+    # Validazione username unico
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
@@ -54,41 +54,44 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
+    # Campo per l'email con lunghezza minima e massima, deve essere una email valida, non può essere vuoto
     email = StringField('Email', validators=[
         Length(min=2, max=100), Email(), DataRequired()])
+    # Campo per la password con lunghezza minima e massima, non può essere vuoto
     password = PasswordField('Password', validators=[
         Length(min=8, max=100), DataRequired()])
+    # Checkbox per ricordarsi l'accesso
     remember_me = BooleanField('Remember Me')
+    # Bottone per il login
     submit = SubmitField('Log In')
 
 
 class UpdateUserForm(FlaskForm):
+    # Campo per l'username con lunghezza minima e massima, non può essere vuoto
     username = StringField('Username', validators=[
         Length(min=2, max=30), DataRequired()])
-    #    email = EmailField('email', validators=[Length(2, 100)])
+    # Campo per l'email con lunghezza minima e massima, deve essere una email valida, non può essere vuoto
     email = StringField('Email', validators=[
         Length(min=2, max=100), Email(), DataRequired()])
+    # Campo per caricare l'immagine dell'utente con estensioni ammesse
     image_file = FileField('Update your Avatar', validators=[
         FileAllowed(['jpg', 'png'])])
-
+    # Bottone per aggiornare il profilo utente
     submit = SubmitField('Update Your Profile!')
 
-    # def validate_field(self, field):
-    #     if True:
-    #         raise ValdationError('Validation Message')
-
+    # Validazione email unica
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email already registered')
 
+    # Validazione username unico
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('Username already registered')
-
 
 class NewPostForm(FlaskForm):
     post_title = StringField('Post Title', validators=[
